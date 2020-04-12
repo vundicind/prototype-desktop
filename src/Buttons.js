bd38128268ef1cdb4eafedda65fb599fce90aca3
Radu Dumbraveanu <vundicind@gmail.com>
2020-04-13 01:03:34 +0300

    Consume REST API

--- src/Buttons.js
+++ src/Buttons.js
@@ -2,6 +2,36 @@ import React, { Component } from 'react';
 import './Buttons.css';
 
 class Buttons extends Component {
+  state = {
+    error: null,
+    isLoaded: false,
+    items: []
+  };
+
+  handleClick(value) {
+    const request = new Request('http://localhost:8080/buttons/' + value, {method: 'POST'});
+    fetch(request)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.items
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+    this.props.onButtonChange(value)
+  }
+
   render() {
     const buttons = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
     return (
@@ -10,7 +40,7 @@ class Buttons extends Component {
           {buttons.map((value, index) => {
             return (
               <div className="form-row">
-                <button type="button" className="btn btn-primary" onClick={() => this.props.onButtonChange(value) }>{value}</button>
+                <button type="button" className="btn btn-primary" onClick={() => this.handleClick(value)}>{value}</button>
               </div>
             )
           })}
